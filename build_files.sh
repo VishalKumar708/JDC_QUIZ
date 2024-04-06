8927419dc759be81b55e6f525d66ef83a5cd5907
vishal-code123 <vk4499573@gmail.com>
2024-04-06 22:11:39 +0530

    update again .sh file

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+echo "deployment execution start...."
 # Locate Python Path
 python_path=$(which python3.9)
+echo "Python Path: $python_path"
 
+# Install Django
+$python_path -m pip install django
 # Install project dependencies from requirements.txt
 $python_path -m pip install -r requirements.txt
 
