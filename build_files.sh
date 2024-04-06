fddec468c4c7577a8ffded25814c4ddfd5b7652d
vishal-code123 <vk4499573@gmail.com>
2024-04-06 22:20:03 +0530

    update again .sh file

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# Install pip if not already installed
 echo "deployment execution start...."
+if ! command -v pip &>/dev/null; then
+    echo "Installing pip..."
+    sudo apt-get install python3-pip
+fi
+
 # Locate Python Path
 python_path=$(which python3.9)
 echo "Python Path: $python_path"
 
 # Install Django
 $python_path -m pip install django
+
 # Install project dependencies from requirements.txt
 $python_path -m pip install -r requirements.txt
 
