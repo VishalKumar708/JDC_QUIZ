2756d22ff913cadf0317308f59626216e8d7bd5d
vishal-code123 <vk4499573@gmail.com>
2024-04-06 21:27:01 +0530

    update .sh file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
-# Install project dependencies from requirements.txt
+# Update package manager and install Python 3.9
+sudo apt-get update
+sudo apt-get install python3.9 python3.9-distutils
+
+# Install pip for Python 3.9
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+
+# Install project dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
